070360347c15d4fa50d1e424a67f3baa18faad78
19840769 <kodikarah.21@uom.lk>
2025-05-05 10:40:58 +0530

    (change)

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\admin\DashboardController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+ Route :: post('authenticate',[AuthenticationController::class,'authenticate']);
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route :: get('dashbaord',[DashboardController::class,'index']);
+    Route :: get('logout',[DashboardController::class,'index']);
+
+
+});
